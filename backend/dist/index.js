e87b6b65cc3173b6e6e9cbb2587283dbd5c898f2
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-02-02 23:10:19 +0530

    Added mongoose

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,10 +6,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+const mongoose_1 = require("mongoose");
+const cors = require('cors');
 const app = (0, express_1.default)();
+const noteSchema = new mongoose_1.Schema({
+    title: String,
+    content: String,
+    author: String
+});
+const Note = (0, mongoose_1.model)('Note', noteSchema);
+const mongoUrl = process.env.MONGO_URL;
+(0, mongoose_1.connect)(mongoUrl);
+app.use(cors());
+app.use(express_1.default.json());
 const PORT = process.env.PORT;
 app.get('/', (req, res) => {
     res.send('Server Running');
+});
+app.get('/api/notes', (request, response) => {
+    Note
+        .find({})
+        .then(notes => {
+        response.json(notes);
+    });
+});
+app.post('/api/notes', (request, response) => {
+    const blog = new Note(request.body);
+    blog
+        .save()
+        .then(result => {
+        response.status(201).json(result);
+    });
 });
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,61 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, response, Response } from 'express';
 import dotenv from 'dotenv'
-
 dotenv.config();
-
+import { Schema, model, connect } from 'mongoose';
+const cors = require('cors')
 const app: Express = express();
+
+interface notes {
+    title: string;
+    content: string;
+    author: string
+}
+
+const noteSchema = new Schema<notes>({
+    title: String,
+    content: String,
+    author: String
+})
+
+const Note = model<notes>('Note', noteSchema)
+
+//look into this later//
+declare const process : {
+    env: {
+      MONGO_URL: string,
+      PORT: number
+    }
+  }
+
+const mongoUrl = process.env.MONGO_URL
+connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
 const PORT = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server Running');
 });
+
+app.get('/api/notes', (request: Request, response: Response) => {
+    Note
+        .find({})
+        .then(notes => {
+            response.json(notes)
+        })
+})
+
+app.post('/api/notes', (request,response) => {
+    const blog = new Note(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
   
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`);

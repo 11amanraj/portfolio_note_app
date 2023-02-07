4b4962a35c259006f198422c188408df5d5b0014
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-02-07 15:08:00 +0530

    Test 2 : Added test database

--- backend/dist/utils/config.js
+++ backend/dist/utils/config.js
@@ -5,8 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
+const MONGO_URL = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGO_URL
+    : process.env.MONGO_URL;
+// const MONGO_URL = process.env.MONGO_URL
 const config = {
     MONGO_URL,
     PORT
--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -5,12 +5,17 @@ dotenv.config()
 declare const process : {
     env: {
       MONGO_URL: string,
-      PORT: number
+      PORT: number,
+      TEST_MONGO_URL: string,
+      NODE_ENV: string
     }
   }
 
-const MONGO_URL = process.env.MONGO_URL
 const PORT = process.env.PORT
+const MONGO_URL = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGO_URL
+    : process.env.MONGO_URL
+// const MONGO_URL = process.env.MONGO_URL
 
 const config = {
     MONGO_URL,

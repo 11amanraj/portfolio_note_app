f9f20ce253db1dd2c11df4406bca5961f42879e4
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-02-14 01:01:47 +0530

    Added a placeholder Loading Component

--- frontend/src/components/DisplaySection/NotesGallery.tsx
+++ frontend/src/components/DisplaySection/NotesGallery.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useContext } from 'react'
 import axios from 'axios';
 import { note, TypeofSelection } from '../../shared/interfaces/notes'
 import SelectionContext from '../../store/selection-context';
+import Loading from '../UI/Loading';
 
 const NotesGallery: React.FC<{id: string | undefined}> = ({id}) => {
     const [notes, setNotes] = useState<note[]>([])
@@ -24,26 +25,20 @@ const NotesGallery: React.FC<{id: string | undefined}> = ({id}) => {
             })
     }, [id])
 
-    if (loading) {
-        return (
-            <div>
-                Loading....
-            </div>
-        )
-    } else {
-        return (
-            <div className={styles.container}>
-                {notes.map(note => (
-                    <div key={note.id} className={styles.note}>
-                        <div>
-                            <h2>{note.title}</h2>
-                            <p>{`by ${note.author}`}</p>
-                        </div>
+    if (loading) return <Loading />
+
+    return (
+        <div className={styles.container}>
+            {notes.map(note => (
+                <div key={note.id} className={styles.note}>
+                    <div>
+                        <h2>{note.title}</h2>
+                        <p>{`by ${note.author}`}</p>
                     </div>
-                ))}
-            </div>
-        )
-    }
+                </div>
+            ))}
+        </div>
+    )
 
 }
  

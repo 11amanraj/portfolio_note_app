12ad5313bcdc24a46c73678cc0f1b1a67cf91ee1
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-02-13 12:35:37 +0530

    Working on note section

--- frontend/src/components/DetailedSection.tsx
+++ frontend/src/components/DetailedSection.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { notebook } from '../shared/interfaces/notes';
 import styles from './DetailedSection.module.css'
 import NotesGallery from './DisplaySection/NotesGallery';
 
 const DetailedSection: React.FC<{notebook: notebook | null}> = ({notebook}) => {
+    const [selectedNote, setSelectedNote] = useState<string | null>(null)
+
     const openNoteHandler = (id: string) => {
         console.log(id)
+        setSelectedNote(id)
     }
 
-    if(notebook !== null) {
+    if(selectedNote !== null) {
+        return (
+            <div>
+                {selectedNote}
+            </div>
+        )
+    } else if(notebook !== null) {
         return (
             <NotesGallery onSelect={openNoteHandler} notebook={notebook}/>
         )
@@ -16,9 +26,8 @@ const DetailedSection: React.FC<{notebook: notebook | null}> = ({notebook}) => {
             <div>
                 Please Select A Notebook
             </div>
-        )
-        
-    }
+        )   
+    } 
 }
  
 export default DetailedSection;

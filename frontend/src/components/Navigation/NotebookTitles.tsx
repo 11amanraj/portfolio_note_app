ba2331751b7d6de132b43dbddf51c2c6e9849c48
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-02-14 01:56:34 +0530

    Still on Sidebar

--- frontend/src/components/Navigation/NotebookTitles.tsx
+++ frontend/src/components/Navigation/NotebookTitles.tsx
@@ -1,18 +1,18 @@
 import styles from './NotebookTitles.module.css'
 import { NavLink } from 'react-router-dom'
 import { notebook } from '../../shared/interfaces/notes'
+import NotesTitle from './NotesTitle'
 
 const NotebookTitles: React.FC<{notebook: notebook}> = ({notebook}) => {
-    console.log(notebook)
-
     return ( 
         <NavLink 
             className={({isActive}) => isActive ? styles.active : undefined} 
             to={`/notebook/${notebook.id}`}>
                 <p className={styles.title}>{notebook.title}</p>
                 <div className={styles.notes}>
-                    <p>{notebook.notes[0].title}</p>
-                    <p>{notebook.notes[1].title}</p>
+                    {notebook.notes.map(note => (
+                        <NotesTitle key={note.id} note={note}/>
+                    ))}
                 </div>
         </NavLink>
      );

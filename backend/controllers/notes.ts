9e82a63299453081496541d7462b6c24356a88ad
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-02-25 13:55:27 +0530

    added error catching in notes controller

--- backend/controllers/notes.ts
+++ backend/controllers/notes.ts
@@ -4,16 +4,25 @@ import Note from '../models/note'
 import Notebook from '../models/notebook'
 
 
-notesRouter.get('/', (request: Request, response: Response, next: NextFunction) => {
-    Note
-        .find({})
-        // .sort({ dateCreated: -1 }) 
-        .populate('notebook', { title: 1, id: 1 })
-        .then((notes) => {
-            console.log(notes)
-            response.json(notes)
-        })
-        .catch((error) => next(error))
+notesRouter.get('/', async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const notes = await Note
+            .find({})
+            // .sort({ dateCreated: -1 }) 
+            .populate('notebook', { title: 1, id: 1 })
+
+        response.json(notes)
+    } catch(error) {
+        next(error)
+    }
+    // Note
+    //     .find({})
+    //     // .sort({ dateCreated: -1 }) 
+    //     .populate('notebook', { title: 1, id: 1 })
+    //     .then((notes) => {
+    //         response.json(notes)
+    //     })
+    //     .catch((error) => next(error))
 })
 
 // notesRouter.get('/important', (request: Request, response: Response, next: NextFunction) => {
@@ -28,47 +37,59 @@ notesRouter.get('/', (request: Request, response: Response, next: NextFunction) 
 //         .catch((error) => next(error))
 // })
 
-notesRouter.get('/:id', (request: Request, response: Response, next: NextFunction) => {
-    Note
-        .findById(request.params.id)
-        .then((notes) => {
-            response.json(notes)
-        })
-        .catch((error) => next(error))
+notesRouter.get('/:id', async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const note = await Note.findById(request.params.id)
+        response.json(note)
+    } catch(error: any) {
+        if(error.name === 'CastError') {
+            return response.status(400).json({message:'Notebook does not exist'})
+        } else {
+            next(error)
+        }
+    } 
 })
 
 notesRouter.post('/', async (request: Request, response: Response, next: NextFunction) => {
-    const notebook = await Notebook.findById(request.body.notebookID) 
-
-    if (notebook === null) {
-        console.log('error')
-        throw new Error('should be populated')  
-    } else {
-        const note = new Note({
-            title: request.body.title,
-            content: request.body.content,
-            author: request.body.author,
-            pinned: false,
-            dateCreated: new Date(),
-            notebook: notebook._id
-        })
-
-        const savedNote = await note.save()
-        response.status(201).json(savedNote)
-
-        if(Array.isArray(notebook.notes)) {
-            notebook.notes.push(savedNote._id)
-            await notebook.save()
+    try {
+        const notebook = await Notebook.findById(request.body.notebookID) 
+    
+        if (notebook === null) {
+            console.log('error')
+            throw new Error('should be populated')  
+        } else {
+            const note = new Note({
+                title: request.body.title,
+                content: request.body.content,
+                author: request.body.author,
+                pinned: false,
+                dateCreated: new Date(),
+                notebook: notebook._id
+            })
+    
+            const savedNote = await note.save()
+            response.status(201).json(savedNote)
+    
+            if(Array.isArray(notebook.notes)) {
+                notebook.notes.push(savedNote._id)
+                await notebook.save()
+            }
         }
+
+    } catch(error) {
+        next(error)
     }
 })
 
 notesRouter.delete('/:id', async (request: Request, response: Response, next: NextFunction) => {
     // deleting note also removes the note id from associated notebook
 
-    Note
-        .findByIdAndDelete(request.params.id)
-        .then(() => response.status(204).end())
+    try {
+        await Note.findByIdAndDelete(request.params.id)
+        return response.status(204).end()
+    } catch(error) {
+        next(error)
+    }
 })
 
 export default notesRouter

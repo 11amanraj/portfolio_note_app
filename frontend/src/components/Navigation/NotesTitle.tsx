881ab4edcfc911dc7a3e9494ab461724b3bd152a
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-02-27 19:20:02 +0530

    Added delete note func to DeleteEntry component

--- frontend/src/components/Navigation/NotesTitle.tsx
+++ frontend/src/components/Navigation/NotesTitle.tsx
@@ -1,14 +1,35 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { note } from '../../shared/interfaces/notes';
+import DeleteEntry from '../Operations/DeleteEntry';
 import styles from './NotesTitle.module.css'
+import axios from 'axios';
+import { useContext } from 'react'
+import { NotebooksContext } from '../../store/NotebooksContextProvider';
 
 const NotesTitle: React.FC<{note: note}> = ({note}) => {
+    const { rerenderComponent } = useContext(NotebooksContext)
+    const navigate = useNavigate()
+
+    const deleteHandler = (id: string) => {
+        console.log(id)
+        axios
+            .delete(`http://localhost:8000/api/notes/${id}`)
+            .then(response => {
+                rerenderComponent(true)
+                navigate(`/notebook/${note.notebook}`)
+            })
+            .catch(error => console.log(error))
+    }
+
     return (
         <>
             <NavLink 
                 className={({isActive}) => isActive ? styles.active : undefined}
                 to={`/notebook/${note.notebook}/note/${note.id}`}>
-                    <p>{note.title}</p>
+                        <div className={styles.title}>
+                            <span>{note.title}</span>
+                            <DeleteEntry onDelete={deleteHandler} id={note.id}/>
+                        </div>
             </NavLink> 
         </> 
      );

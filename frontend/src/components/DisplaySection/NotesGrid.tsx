d8821b45b98f5d527a9f7923a8fe16b55312a34f
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-03-11 11:31:20 +0530

    Minor Changes

--- frontend/src/components/DisplaySection/NotesGrid.tsx
+++ frontend/src/components/DisplaySection/NotesGrid.tsx
@@ -3,10 +3,13 @@ import { note } from '../../shared/interfaces/notes';
 import styles from './NotesGrid.module.css'
 
 const NotesGrid: React.FC<{notes: note[]}> = ({notes}) => {
+    // add grid details in database itself
+
     return ( 
         <div className={styles.container}>
             {notes.map(note => (
                 <div key={note.id}>
+                {/* <div className={`${styles.note} ${styles[note.size]}`}> */}
                     <Link to={`/notebook/${note.notebook}/note/${note.id}`} className={styles.inner}>
                         <h2>{note.title}</h2>
                         <p>{`by ${note.author}`}</p>

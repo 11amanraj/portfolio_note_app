5d4b9520f9b7cb2c129e124ce819bb3401266c39
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-04-18 19:54:46 +0530

    Minor changes

--- frontend/src/pages/Note.tsx
+++ frontend/src/pages/Note.tsx
@@ -1,15 +1,22 @@
 import { useParams } from 'react-router-dom'
 import DetailedNote from '../components/DisplaySection/DetailedNote';
 import SideBar from '../components/Navigation/Sidebar';
+import EachPage from './EachPage';
 
 const Note = () => {
     const params = useParams()
 
+    // return (
+    //     <>
+    //         <SideBar />
+    //         <DetailedNote id={params.noteid}/>
+    //     </>
+    // )
+
     return (
-        <>
-            <SideBar />
+        <EachPage>
             <DetailedNote id={params.noteid}/>
-        </>
+        </EachPage>
     )
 
 }
